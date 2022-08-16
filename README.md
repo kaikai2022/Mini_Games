546fa4b9243cc1959033a2847f2ff1a4c10a30b9
xk <xiaokai@xk.com>
2022-08-16 14:33:10 +0700

    readme

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
-
-
-
-
 引擎版本： Cocos2dx-lua 3.10
-
-
 
 ##### 开发需求
 
 - 使用纯lua编写游戏，不要修改及增加coocs2dx引擎c++代码
 - 游戏资源脚本无需加密、混淆
 - 无需过度封装
-
-
 
 ##### Lua开发
 
@@ -25,31 +17,25 @@ $ ./run.sh
 
 使用`cmd + r` 快速重启模拟器，修改`run.sh`可以自定义分辨率等，具体看该文件。
 
-
-
 ##### Lua调试
 
 推荐使用`VSCode`进行开发， 下载`luaide-lite`可以调试lua
 
-
-
 ##### iOS真机
 
-1. 使用Xcode打开文件`proj_lua_lite/frameworks/runtime-src/proj.ios_mac/project.xcodeproj` 
+1. 使用Xcode打开文件`proj_lua_lite/frameworks/runtime-src/proj.ios_mac/project.xcodeproj`
 2. 添加调试账号
 3. 设置bundie id
 4. 运行即可
-
-
 
 ##### Android真机
 
 ###### run
 
 1. 使用Android Studiod打开目录`proj_lua_lite/frameworks/runtime-src/proj.android-studio`
-2.  `local.properties` 文件中配置本地SDK目录`sdk.dir=sdkPath`
+2. `local.properties` 文件中配置本地SDK目录`sdk.dir=sdkPath`
 3. 下载NDK `android-ndk-r14b`
-4.  `local.properties` 文件中配置本地ndk目录 `ndk.dir=ndkPath` 
+4. `local.properties` 文件中配置本地ndk目录 `ndk.dir=ndkPath`
 5. 点击菜单`File/Sync Project with Gradle Files` 同步gradle文件
 6. ** 注意因为 `.externalNativeBuild` 文件夹是忽略提交的，所以额外的需要点击一下菜单栏 `Build/refresh lingked C++ projects` **
 7. 点击`run`或者`debug`按钮即可运行游戏
@@ -64,8 +50,6 @@ $ ./run.sh
 > 如果是使用M1的新macbook麻烦注释掉工程中 `app/build.gradle` 中的 externalNativeBuild配置 + ndk配置。
 > 将 `sourceSets.main` 中 `jniLibs.srcDirs` 行开启，使用已经编译号的so库进行出包即可。
 > 若不是M1的macbook，有修改C++代码的需求或调试C++情况，可以启用 `app/build.gradle`中的 externalNativeBuild配置 + ndk配置，注释`jniLibs.srcDirs`行自行编译so。
-
-
 
 ##### 游戏开发
 
@@ -85,15 +69,11 @@ display.realCx = display.cy
 display.realCy = display.cx
 ```
 
-
-
 ##### 交付规范
 
 - 游戏内容需适配iphone和ipad
 - 游戏资源代码存放在单一目录，目录按游戏名称，例如游戏名称为NumberMerge,，则代码放在src/NumberMerge，资源放在res/NumberMerge
 - 游戏lua入口文件固定为MainScene.lua
-
-
 
 ##### 交付资源目录
 
@@ -110,3 +90,19 @@ NumberMerge
     |--NumberMerge.appiconset
 ```
 
+##### GoogleADMob的接入
+
+A.获取GoogleAD的广告id和App的Banner的id。 a.在GoogleABMod（ https://apps.admob.com ) 申请账号并登陆到控制台 b.在控制台新建对应的IOS 平台App (如图：)
+![img.png](Readme.img/1.png)
+c.在创建好的应用中创建-广告单元-->横幅广告
+![img.png](Readme.img/2.png)
+d.复制横幅广告的id ( ca-app-pub-6707280108018024/2595513523 )到
+![img.png](Readme.img/3.png)
+RootViewController.mm 文件
+![img.png](Readme.img/4.png)
+e.复制googleAD中创建app 的 应用id (ca-app-pub-6707280108018024~3009612347) 到
+![img.png](Readme.img/5.png)
+Info.plist
+![img.png](Readme.img/6.png)
+
+    
--- src/Cocos2dxiOSLuaSDK.lua
+++ src/Cocos2dxiOSLuaSDK.lua
@@ -3,62 +3,62 @@ local targetPlatform = cc.Application:getInstance():getTargetPlatform()
 --把样例内容替换成自己SDK的名字
 local Cocos2dxiOSLuaSDK = class("Cocos2dxiOSLuaSDK")
 
---格式为上面声明的名字:函数名 命名无特殊要求，但是此处函数名是cocos2dx开发者会看到并调用的，请做到一眼就能明白作用的效果
-function Cocos2dxiOSLuaSDK:showDetailPageWithProductId(productId, callback)
-	print("call showDetailPageWithProductId start")
-	if (cc.PLATFORM_OS_ANDROID == targetPlatform) then
-        --此处为安卓SDK调用逻辑，因为对于cocos2dx开发者而言，不关心是什么平台
-        --所以需要实现lua调用一个函数自动识别平台并调用对应SDK的目的（安卓/iOS 共用一个lua桥接文件），此处需要给安卓开发者预留空间
-	end
-    if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) then --固定句式
-        local luaoc = require "cocos.cocos2d.luaoc" --固定句式
-        local args = {productId = productId, functionId = callback} --参数只能传字典或者不传,此处传入的回调函数并非函数而只是个标识符
-        --前面是固定句式，第一个参数是OC桥接文件的类名，第二个参数是OC桥接文件中想要调用的方法名，第三个参数是传入的字典
-        local ok, ret = luaoc.callStaticMethod("Cocos2dxiOSLuaSDKBridge","Cocos2dxiOSLuaSDKBridgeSelector",args)
-        if not ok then
-            print("luaoc showDetailPageWithProductId error:"..ret)
-        end
-    end
-end
+----格式为上面声明的名字:函数名 命名无特殊要求，但是此处函数名是cocos2dx开发者会看到并调用的，请做到一眼就能明白作用的效果
+--function Cocos2dxiOSLuaSDK:showDetailPageWithProductId(productId, callback)
+--	print("call showDetailPageWithProductId start")
+--	if (cc.PLATFORM_OS_ANDROID == targetPlatform) then
+--        --此处为安卓SDK调用逻辑，因为对于cocos2dx开发者而言，不关心是什么平台
+--        --所以需要实现lua调用一个函数自动识别平台并调用对应SDK的目的（安卓/iOS 共用一个lua桥接文件），此处需要给安卓开发者预留空间
+--	end
+--    if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) then --固定句式
+--        local luaoc = require "cocos.cocos2d.luaoc" --固定句式
+--        local args = {productId = productId, functionId = callback} --参数只能传字典或者不传,此处传入的回调函数并非函数而只是个标识符
+--        --前面是固定句式，第一个参数是OC桥接文件的类名，第二个参数是OC桥接文件中想要调用的方法名，第三个参数是传入的字典
+--        local ok, ret = luaoc.callStaticMethod("Cocos2dxiOSLuaSDKBridge","Cocos2dxiOSLuaSDKBridgeSelector",args)
+--        if not ok then
+--            print("luaoc showDetailPageWithProductId error:"..ret)
+--        end
+--    end
+--end
+--
+--function Cocos2dxiOSLuaSDK:showDetailPage()
+--	print("call showDetailPage start")
+--	if (cc.PLATFORM_OS_ANDROID == targetPlatform) then
+--
+--	end
+--    if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) then
+--        local luaoc = require "cocos.cocos2d.luaoc"
+--        local ok, ret = luaoc.callStaticMethod("Cocos2dxiOSLuaSDKBridge","Cocos2dxiOSLuaSDKBridgeNoParameterSelector")
+--        if not ok then
+--            print("luaoc showDetailPage error:"..ret)
+--        end
+--    end
+--end
 
-function Cocos2dxiOSLuaSDK:showDetailPage()
-	print("call showDetailPage start")
-	if (cc.PLATFORM_OS_ANDROID == targetPlatform) then
 
-	end
-    if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) then
-        local luaoc = require "cocos.cocos2d.luaoc"
-        local ok, ret = luaoc.callStaticMethod("Cocos2dxiOSLuaSDKBridge","Cocos2dxiOSLuaSDKBridgeNoParameterSelector")
-        if not ok then
-            print("luaoc showDetailPage error:"..ret)
-        end
-    end
-end
-
-
-
+---@public hideGodAdBannerView 隐藏googleADBanner
 function Cocos2dxiOSLuaSDK:hideGodAdBannerView()
     if (cc.PLATFORM_OS_ANDROID == targetPlatform) then
 
     end
     if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) then
         local luaoc = require "cocos.cocos2d.luaoc"
-        local ok, ret = luaoc.callStaticMethod("GoogleAdModSDK","hideBannerView")
+        local ok, ret = luaoc.callStaticMethod("GoogleAdModSDK", "hideBannerView")
         if not ok then
-            print("luaoc showDetailPage error:"..ret)
+            print("luaoc showDetailPage error:" .. ret)
         end
     end
 end
-
+---@public showGodAdBannerView 显示隐藏googleADBanner
 function Cocos2dxiOSLuaSDK:showGodAdBannerView()
     if (cc.PLATFORM_OS_ANDROID == targetPlatform) then
 
     end
     if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) then
         local luaoc = require "cocos.cocos2d.luaoc"
-        local ok, ret = luaoc.callStaticMethod("GoogleAdModSDK","showBannerView")
+        local ok, ret = luaoc.callStaticMethod("GoogleAdModSDK", "showBannerView")
         if not ok then
-            print("luaoc showDetailPage error:"..ret)
+            print("luaoc showDetailPage error:" .. ret)
         end
     end
 end
